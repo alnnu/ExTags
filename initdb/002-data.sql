42ee00ba2cbf1ff9799c9fce2127f767db7f0a2d
LuannFelipe <luannf1020@gmail.com>
2022-07-27 18:21:04 -0300

    adicão da pagina para adicionar tarefa e ajuste na passagem de parâmetro para criar uma tarefa

---
+++ initdb/002-data.sql
@@ -0,0 +1,116 @@
+CREATE TABLE pessoa (
+    email text PRIMARY KEY,
+    password text,
+    username text
+);
+
+
+CREATE TABLE Projeto (
+    nome text,
+    id SERIAL PRIMARY KEY,
+    dia DECIMAL,
+    mes DECIMAL,
+    ano DECIMAL,
+    estado DECIMAL,
+    gerente_email text
+);
+
+ALTER TABLE Projeto ADD CONSTRAINT FK_Projeto_2
+    FOREIGN KEY (gerente_email)
+    REFERENCES pessoa (email)
+    ON DELETE CASCADE;
+
+
+CREATE TABLE participa (
+    pessoa_email text,
+    Projeto_id int,
+	CONSTRAINT pk_par primary key(pessoa_email,Projeto_id)
+);
+
+ALTER TABLE participa ADD CONSTRAINT FK_participa_1
+    FOREIGN KEY (pessoa_email)
+    REFERENCES pessoa (email)
+    ON DELETE CASCADE;
+
+ALTER TABLE participa ADD CONSTRAINT FK_participa_2
+    FOREIGN KEY (Projeto_id)
+    REFERENCES Projeto (id)
+    ON DELETE CASCADE;
+
+
+CREATE TABLE nota (
+    nome text,
+    texto text,
+    data DATE,
+    id SERIAL PRIMARY KEY,
+    Projeto_id int,
+    pessoa_email text
+);
+
+ALTER TABLE nota ADD CONSTRAINT FK_nota_2
+    FOREIGN KEY (Projeto_id)
+    REFERENCES Projeto (id)
+    ON DELETE CASCADE;
+
+ALTER TABLE nota ADD CONSTRAINT FK_nota_3
+    FOREIGN KEY (pessoa_email)
+    REFERENCES pessoa (email)
+    ON DELETE CASCADE;
+
+CREATE TABLE Tarefa (
+    data DATE,
+    descricao text,
+    prioridade DECIMAL,
+    estado DECIMAL,
+    estimativa DECIMAL,
+    id SERIAL PRIMARY KEY,
+    nome text,
+    Projeto_id int,
+    pessoa_email text
+);
+
+ALTER TABLE Tarefa ADD CONSTRAINT FK_Tarefa_2
+    FOREIGN KEY (Projeto_id)
+    REFERENCES Projeto (id)
+    ON DELETE CASCADE;
+
+ALTER TABLE Tarefa ADD CONSTRAINT FK_Tarefa_3
+    FOREIGN KEY (pessoa_email)
+    REFERENCES pessoa (email);
+
+
+CREATE TABLE processo (
+    log text,
+    log_data text,
+    id SERIAL PRIMARY KEY,
+    pessoa_email text,
+    Tarefa_id int
+);
+
+ALTER TABLE processo ADD CONSTRAINT FK_processo_2
+    FOREIGN KEY (pessoa_email)
+    REFERENCES pessoa (email)
+    ON DELETE CASCADE;
+
+ALTER TABLE processo ADD CONSTRAINT FK_processo_3
+    FOREIGN KEY (Tarefa_id)
+    REFERENCES Tarefa (id)
+    ON DELETE CASCADE;
+
+
+CREATE TABLE faz (
+	id SERIAL PRIMARY KEY,
+    Tarefa_id int,
+    pessoa_email text
+);
+
+ALTER TABLE faz ADD CONSTRAINT FK_faz_1
+    FOREIGN KEY (Tarefa_id)
+    REFERENCES Tarefa (id)
+    ON DELETE SET NULL;
+
+ALTER TABLE faz ADD CONSTRAINT FK_faz_2
+    FOREIGN KEY (pessoa_email)
+    REFERENCES pessoa (email)
+    ON DELETE SET NULL;
+/*drop table faz, nota,participa,pessoa,processo,projeto,tarefa*/
--- src/controls/Tarefa.controller.js
+++ src/controls/Tarefa.controller.js
@@ -3,7 +3,7 @@ const Tarefa = require("../nodel/Tarefa.model");
 const route = new Router();
 
 route
-  .get("/api/tarefa", async (ctx) => {
+  .get("tarefa", async (ctx) => {
     const tarefa = new Tarefa();
     if (ctx.query.id != null) {
       let tarefas = projeto.getTarefaById(ctx.query.id);
@@ -13,14 +13,20 @@ route
       ctx.body = await tarefa;
     }
   })
-  .post("/api/tarefa/criar", async (ctx) => {
-    ctx.checkBody("estado").ge(1).le(3).notEmpty().notBlank();
+  .get("/projeto/addtarefa", async (ctx) => {
+    if (ctx.query.projeto) {
+      await ctx.render("addTarefa", {
+        projeto_id: ctx.query.projeto
+      });
+    } else {
+      ctx.status = 404;
+    }
+  })
+  .post("/projeto/addtarefa", async (ctx) => {
     ctx.checkBody("prioridade").ge(1).le(3).notEmpty().notBlank();
     ctx.checkBody("estimativa").isNumeric().notEmpty().notBlank();
     ctx.checkBody("nome").notEmpty().notBlank();
     ctx.checkBody("projeto").notEmpty().notBlank();
-    ctx.checkBody("pessoa").notEmpty().notBlank();
-    ctx.checkBody("data").isDate();
 
     if (ctx.errors) {
       ctx.body = ctx.errors;
@@ -28,16 +34,18 @@ route
     } else {
       const novaTarefaOBJ = ctx.request.body;
       const tarefa = new Tarefa();
+      const data = new Date().toISOString().substring(0, 10);
       ctx.body = await tarefa.criar(
-        novaTarefaOBJ.data,
+        data,
         novaTarefaOBJ.descricao,
         novaTarefaOBJ.prioridade,
-        novaTarefaOBJ.estado,
+        1,
         novaTarefaOBJ.estimativa,
         novaTarefaOBJ.nome,
-        novaTarefaOBJ.projeto,
-        novaTarefaOBJ.pessoa
+        ctx.query.projeto,
+        ctx.state.user.email
       );
+      ctx.body = ctx.request.body
     }
   })
   .delete("/api/tarefa/deletar", async (ctx) => {
